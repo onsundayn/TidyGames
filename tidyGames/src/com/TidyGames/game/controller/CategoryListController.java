003a2621d3f759b40af11ffc94c65c5569428f08
LEEHR11 <87908533+LEEHR11@users.noreply.github.com>
2021-12-15 16:26:37 +0900

    CategoryList select구현

--- tidyGames/src/com/TidyGames/game/controller/CategoryListController.java
+++ tidyGames/src/com/TidyGames/game/controller/CategoryListController.java
@@ -1,11 +1,17 @@
 package com.TidyGames.game.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.TidyGames.game.model.service.GameService;
+import com.TidyGames.game.model.vo.Category;
+
 
 /**
  * Servlet implementation class CategoryList
@@ -26,6 +32,10 @@ public class CategoryListController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ArrayList<Category> list = new GameService().selectCategoryList();
+		
+		request.setAttribute("list", list);
 		
 		request.getRequestDispatcher("views/game/categoryList.jsp").forward(request, response);
 		
---
+++ tidyGames/src/com/TidyGames/game/model/vo/Category.java
@@ -0,0 +1,58 @@
+package com.TidyGames.game.model.vo;
+
+public class Category {
+
+	private int categoryNo;
+	private String categoryName;
+	private String categoryStatus;
+
+
+	public Category() {}
+
+
+	public Category(int categoryNo, String categoryName, String categoryStatus) {
+		super();
+		this.categoryNo = categoryNo;
+		this.categoryName = categoryName;
+		this.categoryStatus = categoryStatus;
+	}
+
+
+	public int getCategoryNo() {
+		return categoryNo;
+	}
+
+
+	public void setCategoryNo(int categoryNo) {
+		this.categoryNo = categoryNo;
+	}
+
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+
+	public String getCategoryStatus() {
+		return categoryStatus;
+	}
+
+
+	public void setCategoryStatus(String categoryStatus) {
+		this.categoryStatus = categoryStatus;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Category [categoryNo=" + categoryNo + ", categoryName=" + categoryName + ", categoryStatus="
+				+ categoryStatus + "]";
+	}
+	
+	
+}
